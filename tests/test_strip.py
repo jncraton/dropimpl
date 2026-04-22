89214d5b583f7333d6d028dd046d65a96ef7022c
Jon Craton <jncraton@gmail.com>
2026-04-22 15:23:54 -0400

    Add tests/test_strip.py

---
+++ tests/test_strip.py
@@ -0,0 +1,13 @@
+import sys, os
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+from dropimpl.core import strip_function_bodies
+
+
+def test_strip_basic():
+    code = """def add(a, b):
+    'adds things'
+    return a + b
+"""
+    out = strip_function_bodies(code, ['add'])
+    assert "pass" in out
+    assert "adds things" in out
